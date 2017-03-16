fab28c22543b0690c743bb889840428f8ff4aab3
Memo Khoury <majd.khoury93@gmail.com>
2017-03-15 22:13:51 -0400

    added player movement, set players to atlanta, and added all map cities with neighbour nodes

--- Pandemic/Player.cpp
+++ Pandemic/Player.cpp
@@ -50,3 +50,82 @@ Pawn* Player::getPawn()
 {
 	return pawn;
 }
+
+void Player::move()
+{
+	int playerChoice;
+	std::cout << "Here is the list of " << currentCity->getName() << "'s neighbours: " << std::endl;
+	currentCity->getListOfNeighbours();
+	std::cout << "Where would you like to move? (pick a number): " << std::endl;
+	std::cin >> playerChoice;
+
+	while ((!std::cin >> playerChoice) && (playerChoice < 1 || playerChoice > currentCity->getNeighbours().size()))
+	{
+	}
+
+	switch (playerChoice)
+	{
+	case 1:
+		std::cout << name << " is moving to  " << currentCity->getNeighbours().at(0)->getName() << " . . " << std::endl;
+
+		setCurrentCity(currentCity->getNeighbours().at(0));
+		std::cout << "You are now in: " << currentCity->getName() << std::endl;
+		/*std::cout << currentCity->getName() << "'s neighbours are: " << std::endl;
+		currentCity->getListOfNeighbours();*/
+		break;
+
+	case 2:
+		std::cout << name << " is moving to " << currentCity->getNeighbours().at(1)->getName() << " . . " << std::endl;
+		setCurrentCity(currentCity->getNeighbours().at(1));
+		std::cout << "You are now in: " << currentCity->getName() << std::endl;
+		/*std::cout << currentCity->getName() << "'s neighbours are: " << std::endl;
+		currentCity->getListOfNeighbours();*/
+		break;
+
+	case 3:
+		std::cout << name << " is moving to " << currentCity->getNeighbours().at(2)->getName() << " . . " << std::endl;
+		setCurrentCity(currentCity->getNeighbours().at(2));
+		std::cout << "You are now in: " << currentCity->getName() << std::endl;
+		//std::cout << currentCity_h->getName() << "'s neighbours are: " << std::endl;
+		//currentCity_h->getListOfNeighbours();
+		break;
+	case 4:
+		std::cout << name << " is moving to " << currentCity->getNeighbours().at(3)->getName() << " . . " << std::endl;
+		setCurrentCity(currentCity->getNeighbours().at(3));
+		std::cout << "You are now in: " << currentCity->getName() << std::endl;
+		//std::cout << currentCity_h->getName() << "'s neighbours are: " << std::endl;
+		//currentCity_h->getListOfNeighbours();
+		break;
+
+	//case 5:
+	//	std::cout << name_h << " is moving to " << currentCity_h->getNeighbours().at(4)->getName() << " . . " << std::endl;
+	//	currentCity_h = currentCity_h->getNeighbours().at(4);
+	//	std::cout << "You are now in: " << currentCity_h->getName() << std::endl;
+	//	//std::cout << currentCity_h->getName() << "'s neighbours are: " << std::endl;
+	//	//currentCity_h->getListOfNeighbours();
+	//	break;
+	//case 6:
+	//	std::cout << name_h << " is moving to " << currentCity_h->getNeighbours().at(5)->getName() << " . . " << std::endl;
+	//	currentCity_h = currentCity_h->getNeighbours().at(5);
+	//	std::cout << "You are now in: " << currentCity_h->getName() << std::endl;
+	//	//std::cout << currentCity_h->getName() << "'s neighbours are: " << std::endl;
+	//	//currentCity_h->getListOfNeighbours();
+	//	break;
+
+	case 0:
+
+		break;
+
+	}
+}
+
+
+void Player::setCurrentCity(MapCity* currentCity)
+{
+	this->currentCity = currentCity;
+}
+
+std::string Player::getCurrentCity()
+{
+	return currentCity->getName();
+}
